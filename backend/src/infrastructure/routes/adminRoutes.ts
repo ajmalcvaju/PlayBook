4091da41d23231df53ff4c630450a3315ca50d12
ajmalcvaju <ajmalmayanad@gmail.com>
2025-02-09 09:03:28 +0530

    next-admin-side

--- backend/src/infrastructure/routes/adminRoutes.ts
+++ backend/src/infrastructure/routes/adminRoutes.ts
@@ -9,10 +9,11 @@ router.get("/refresh-token",adminController.refreshToken)
 
 router.get("/get-users",authenticateToken,authorizeRoles(["admin"]),adminController.getUser)
 router.get("/get-turfs",authenticateToken,authorizeRoles(["admin"]),adminController.getTurf)
-router.get("/get-booking",authenticateToken,authorizeRoles(["admin"]),adminController.getBookings)
-router.post("/pay-balance",authenticateToken,authorizeRoles(["admin"]),adminController.payBalance)
 router.patch("/block-user",authenticateToken,authorizeRoles(["admin"]),adminController.blockUser)
 router.patch("/block-turf",authenticateToken,authorizeRoles(["admin"]),adminController.blockTurf)
+
+router.get("/get-booking",authenticateToken,authorizeRoles(["admin"]),adminController.getBookings)
+router.post("/pay-balance",authenticateToken,authorizeRoles(["admin"]),adminController.payBalance)
 router.get("/reviews",authenticateToken,authorizeRoles(["admin"]),adminController.getReviews)
 router.delete("/delete-review/:id",authenticateToken,authorizeRoles(["admin"]),adminController.deleteReview)
 
