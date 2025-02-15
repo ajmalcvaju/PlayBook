be6bafaba65ca3e541c209d02d9f0231d0382a06
ajmalcvaju <ajmalmayanad@gmail.com>
2025-02-15 06:49:36 +0530

    local-host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:7000',
+        target:'http://api.play-book.xyz:7000/',
         changeOrigin: true,
         secure:false
       }
